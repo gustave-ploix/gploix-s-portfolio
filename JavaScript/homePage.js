46b063a585fb7e8d16a111c34efc32b5714ab43a
gustave ploix <gustaveploix.pro@gmail.com>
2022-04-22 18:26:21 +0200

    renamed and relocalized the different source files

---
+++ JavaScript/homePage.js
@@ -0,0 +1,27 @@
+const container = document.getElementsByClassName("container")[0];
+
+const nameCard = document.getElementsByClassName("name-card")[0];
+
+const text = document.getElementsByClassName("text");
+
+const btn = document.getElementById("btn");
+
+function loadingPage() {
+    nameCard.classList.add("open");
+
+    for (let i = 0; i < text.length; i++) {
+        text[i].classList.add("visible");
+    };
+
+    setTimeout(() => {
+        btn.classList.add("done")
+    }, 2500);
+}
+
+window.addEventListener("load", loadingPage());
+btn.addEventListener("click", () => {
+
+    container.classList.toggle("to-the-left");
+
+});
+
